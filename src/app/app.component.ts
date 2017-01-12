56bfb57025c3ad281d04e228b9b53110c7c8c5be
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2017-01-12 16:53:27 -0600

    ds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
 
- selector: 'app-root',
+ 
+
+selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
