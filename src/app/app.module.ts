a484fda3348799c8fc0dac39817113d5f1fa5aa1
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2017-02-21 22:11:27 -0600

    chanching some code to fix testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SideEffectTestComponent } from './side-effect-test/side-effect-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideEffectTestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
